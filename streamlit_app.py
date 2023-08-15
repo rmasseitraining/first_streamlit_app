7f4a9ac02214d4e58d68065aafb583589a6771bf
rmasseitraining <141667017+rmasseitraining@users.noreply.github.com>
2023-08-15 15:57:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,7 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+# Allow the end user to add a fruit to the list
+my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
+
+
